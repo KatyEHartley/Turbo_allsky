65c3d094f038a6de1fee35eda9d3dbd0f551cd47
Katy <147080396+KatyEHartley@users.noreply.github.com>
2024-01-26 18:35:34 +0000

    Update AllSky.py

--- AllSky.py
+++ AllSky.py
@@ -1,2 +1,26 @@
-import numpy as np
-import matplotlib.pyplot as plt 
+import streamlit as st
+from pathlib import Path
+import asyncio
+from datetime import datetime, timezone
+
+PLACEHOLDER_URL = "https://www.gaithersburgdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.png"
+
+async def watch(image_pl):
+    dir_path = "watch_dir"
+    previous = None
+    while True:
+        images = list(Path(dir_path).glob("*.jpg"))
+        if images:
+            latest = max(images, key=lambda file: file.stat().st_mtime)
+        else:
+            image_pl.image(PLACEHOLDER_URL, use_column_width=True) #,
+            latest = None
+        if latest != previous:
+            image_pl.image(str(latest), use_column_width=True,  caption='%s' % datetime.fromtimestamp(latest.stat().st_mtime, tz=timezone.utc))
+            previous = latest
+        _ = await asyncio.sleep(1)
+
+
+image_pl = st.empty()
+
+asyncio.run(watch(image_pl))
